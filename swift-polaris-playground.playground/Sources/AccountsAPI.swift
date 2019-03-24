c9af7865663d4bf734768efce08e1f62c1147dcf
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 01:20:38 -0400

    sources: Created Requests.swift.

--- swift-polaris-playground.playground/Sources/AccountsAPI.swift
+++ swift-polaris-playground.playground/Sources/AccountsAPI.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // Class AccounstAPI represents the accounts API.
-public class AccountsAPI: NSObject, URLSessionDelegate {
+public class AccountsAPI {
     var URI: String // API lookup path
     
     // init initializes a new AccountsAPI instance.
@@ -13,40 +13,6 @@ public class AccountsAPI: NSObject, URLSessionDelegate {
     public func NewAccount() -> ([String: Any]?, Error?) {
         let requestContents: [String: Any] = [:] // Empty JSON request
         
-        let jsonData = try? JSONSerialization.data(withJSONObject: requestContents) // Get JSON representation
-        
-        var request = URLRequest(url: URL(string: String(format: "%@/NewAccount", URI))!) // Craft request
-        request.httpMethod = "post" // Set HTTP method
-        
-        request.httpBody = jsonData // Set request body
-        
-        var responseJSON: [String: Any]? = nil // Init response buffer
-        
-        var err: Error? = nil // Init error buffer
-        
-        let sem = DispatchSemaphore(value: 0) // Create sync buffer
-        
-        let task = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: OperationQueue.init()).dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else { // Check for errors
-                err = error! // Set error
-                
-                return // Return
-            }
-
-            responseJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any] // Set response JSON
-            
-            sem.signal() // Dispatch signal
-        }
-        
-        task.resume() // Start task
-        
-        sem.wait() // Wait for finish
-        
-        return (responseJSON, err) // Return response
-    }
-    
-    // urlSession forces a self-signed TLS cert.
-    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!)) // Use credential
+        return Requests.MakeRequest(requestURI: String(format: "%@/NewAccount", URI), requestContents: requestContents) // Return response
     }
 }
---
+++ swift-polaris-playground.playground/Sources/Requests.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+// Class Requests implements a set of http request helper methods.
+class Requests: NSObject, URLSessionDelegate {
+    // MakeRequest makes a synchronous request at a given URI, with a given set of JSON contents.
+    public static func MakeRequest(requestURI: String, requestContents: [String: Any]) -> ([String: Any]?, Error?) {
+        let jsonData = try? JSONSerialization.data(withJSONObject: requestContents) // Get JSON representation
+        
+        var request = URLRequest(url: URL(string: requestURI)!) // Craft request
+        request.httpMethod = "post" // Set HTTP method
+        
+        request.httpBody = jsonData // Set request body
+        
+        var responseJSON: [String: Any]? = nil // Init response buffer
+        
+        var err: Error? = nil // Init error buffer
+        
+        let sem = DispatchSemaphore(value: 0) // Create sync buffer
+        
+        let task = URLSession(configuration: URLSessionConfiguration.default, delegate: Requests(), delegateQueue: OperationQueue.init()).dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else { // Check for errors
+                err = error! // Set error
+        
+                return // Return
+            }
+        
+            responseJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any] // Set response JSON
+        
+            sem.signal() // Dispatch signal
+        }
+        
+        task.resume() // Start task
+        
+        sem.wait() // Wait for finish
+        
+        return (responseJSON, err) // Return response
+    }
+    
+    // urlSession forces a self-signed TLS cert.
+    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!)) // Use credential
+    }
+}
