560e0584cac2b9b300e07967642df6c5f7963dad
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 03:15:55 -0400

    sources: Finished implementing transaction API wrapper.

--- swift-polaris-playground.playground/Sources/TransactionAPI.swift
+++ swift-polaris-playground.playground/Sources/TransactionAPI.swift
@@ -11,10 +11,52 @@ public class TransactionAPI {
         URI = "\(apiURI)/twirp/transaction.Transaction" // Set API URI
     }
     
-    // NewTransaction intiializes a new transaction with the given params.
+    // NewTransaction initializes a new transaction with the given params.
     public func NewTransaction(nonce: uint64, amount: Float64, sender: String, recipient: String, parentTransactions: [String], gasLimit: uint64, gasPrice: Int64, payload: String) -> ([String: Any]?, Error?) {
-        let requestContents: [String: Any] = ["nonce": nonce, "amount": Data(String(amount).utf8).base64EncodedString(), "address": sender, "address2": recipient, "transactionHash": parentTransactions, "gasLimit": gasLimit, "gasPrice": gasPrice, "payload": Data(payload.utf8).base64EncodedString()] // Initialize request
+        let requestContents: [String: Any] = ["nonce": nonce, "amount": Data("\(amount)".utf8).base64EncodedString(), "address": sender, "address2": recipient, "transactionHash": parentTransactions, "gasLimit": gasLimit, "gasPrice": gasPrice, "payload": Data(payload.utf8).base64EncodedString()] // Initialize request
         
         return Requests.MakeRequest(requestURI: "\(URI)/NewTransaction", requestContents: requestContents) // Return response
+    }
+    
+    // CalculateTotalValue calculates the total value of a given transaction.
+    public func CalculateTotalValue(transactionHashes: [String]) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["transactionHash": transactionHashes] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/CalculateTotalValue", requestContents: requestContents) // Return response
+    }
+    
+    // SignTransaction signs a given transaction via ECDSA.
+    public func SignTransaction(transactionHash: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["transactionHash": [transactionHash]] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/SignTransaction", requestContents: requestContents) // Return response
+    }
+    
+    // Publish publishes a given transaction.
+    public func Publish(transactionHash: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["transactionHash": [transactionHash]] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/Publish", requestContents: requestContents) // Return response
+    }
+    
+    // SignMessage signs a given message via ECDSA.
+    public func SignMessage(address: String, messageHash: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["address": address, "payload": messageHash] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/SignMessage", requestContents: requestContents) // Return response
+    }
+    
+    // Verify verifies the validity of a given transaction's ECDSA signature.
+    public func Verify(transactionHash: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["transactionHash": [transactionHash]] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/Verify", requestContents: requestContents) // Return response
+    }
+    
+    // String marshals a given transaction to a JSON-encoded string.
+    public func String(transactionHash: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["transactionHash": [transactionHash]] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/String", requestContents: requestContents) // Return response
     }
 }
