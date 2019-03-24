dfe63513ed51b0578f85651050fb3a51e808f35b
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 15:46:50 -0400

    pages: Added pub handling.

---
+++ swift-polaris-playground.playground/Pages/Transaction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+/*:
+ # The Transaction API
+ 
+ Just like the Accounts API, the Transaction API is a child property of the API class. Initialize one to access the Transaction API.
+ */
+
+import Foundation
+import UIKit
+import PlaygroundSupport
+
+var api: API // Init API instance buffer
+
+API.Offline = API.Offline // Just a reminder to set to true for offline capability (make sure to run run_polaris.sh [node needs to be up!])
+
+if API.Offline { // Check is offline
+    api = API(apiURI: "https://localhost:8000") // Initialize API instance
+} else {
+    api = API(apiURI: "https://108.41.124.60:8000") // Initialize API instance
+}
+
+/*:
+ ## Crafting a New Transaction
+ 
+ Unlike creating a new account, creating a transaction does take a few parameters. First and foremost, the NewTransaction() method takes a nonce. A nonce is essentially the index of the transaction in the list of account transactions (prevent replay attacks). Secondly, a transaction takes an amount. For now, we'll go with 0. Furthermore, NewTransaction then takes a sender and recipient. Again, because I'll assume you're on your own private network, we'll just set it to your address. After the recipient and sender, you'll have to provide the parent hash. This can be easily found via the dag API's GetBestTransaction() method. Finally, the tx will take a gasPrice, gasLimit, and payload. A gasPrice is essentially how much you're willing to pay per gas unit. The gasLimit is how much of that unit you're willing to pay. The payload is essentially just a message you want to pass with the transaction.
+*/
+
+/*:
+ * Experiment:
+ Try creating a new transaction via the interface in the assistant view.
+ 
+    - Set a message in the variable below (everyone connected to the network will see it [if on public net])!
+    - Set a new destination address
+*/
+
+let newAccountAddress = api.Accounts.NewAccount().0!["message"] as! String // Initialize new account
+let lastTransactionHash = api.Dag.GetBestTransaction().0!["message"] as! String // Get last tx
+
+/* Change these two as you wish */
+
+var destination = newAccountAddress // Try changing
+var message = "your message" // Try changing
+
+let baseView = DemoView(frame: CGRect(x: 0, y: 0, width: 500, height: 500)) // Bg
+
+if message == "your message" { // Check still default message
+    baseView.Say(s: "Hint: change the message variable!") // Log should try changing message
+}
+
+var transactionHash = api.Transaction.NewTransaction(nonce: 0, amount: 0, sender: newAccountAddress, recipient: destination, parentTransactions: [lastTransactionHash], gasLimit: 0, gasPrice: 0, payload: message).0!["message"] as! String // Initialize transaction
+
+transactionHash = api.Transaction.SignTransaction(transactionHash: transactionHash).0!["message"] as! String // Sign transaction
+api.Transaction.Publish(transactionHash: transactionHash) // Publish
+
+baseView.Say(s: "😼🎉 transaction \(String(transactionHash.prefix(8)) + "...") published!") // Log publish!
+
+PlaygroundPage.current.liveView = baseView // Set live view
+PlaygroundPage.current.needsIndefiniteExecution = true // Keep open
+
--- swift-polaris-playground.playground/Sources/DemoView.swift
+++ swift-polaris-playground.playground/Sources/DemoView.swift
@@ -2,6 +2,9 @@ import Foundation
 import UIKit
 
 public class DemoView: UIView {
+    public static var workingAPI: API? // Init API buffer
+    public var lastTag = 0 // Init tag buffer
+
     /**
      Draws the DemoView.
     */
@@ -19,12 +22,19 @@ public class DemoView: UIView {
      Adds a label to the working DemoView saying a particular thing, s.
     */
     public func Say(s: String) {
+        if lastTag != 0 { // Check has last tag
+            self.viewWithTag(lastTag)!.removeFromSuperview() // Remove last view with tag
+        }
+
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)) // Init label
         label.center = CGPoint(x: self.bounds.width/2, y: self.bounds.height/2) // Set center
 
         label.textAlignment = NSTextAlignment.center // Set text alignment
         label.text = s // Set text
         label.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.medium) // Set font
+        
+        label.tag = 1 // Set tag
+        self.lastTag = 1 // Set last tag
 
         self.addSubview(label) // Add label
     }
@@ -41,6 +51,9 @@ public class DemoView: UIView {
         textField.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.medium) // Set font
         textField.borderStyle = UITextField.BorderStyle.roundedRect // Set border style
         
+        textField.tag = 1 // Set tag
+        self.lastTag = 1 // Set last tag
+        
         self.addSubview(textField) // Add field
     }
     
@@ -56,6 +69,27 @@ public class DemoView: UIView {
         button.setTitle(p, for: .normal) // Set text
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.medium) // Set font
         
+        button.tag = 1 // Set tag
+        self.lastTag = 1 // Set last tag
+        
         self.addSubview(button) // Add field
+    }
+    
+    /**
+     Prompts the user with an action.
+     */
+    public func PromptExtensible(p: String) -> UIButton {
+        let button = UIButton(type: .roundedRect) // Init button
+        
+        button.frame = CGRect(x: 0, y: 0, width: self.bounds.width*0.75, height: 35) // Set frame
+        button.center = CGPoint(x: self.bounds.width/2, y: self.bounds.height/2) // Set center
+        button.titleLabel?.textAlignment = NSTextAlignment.left // Set text alignment
+        button.setTitle(p, for: .normal) // Set text
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.medium) // Set font
+        
+        button.tag = 1 // Set tag
+        self.lastTag = 1 // Set last tag
+        
+        return button // Return button
     }
 }
