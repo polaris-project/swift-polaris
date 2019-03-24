ffd89db2d8b473eb3e1eb16e09687d7719795989
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-23 23:38:04 -0400

    sources: Created AccountsAPI.swift.

--- swift-polaris-playground.playground/Sources/API.swift
+++ swift-polaris-playground.playground/Sources/API.swift
@@ -1,2 +1,15 @@
 import Foundation
 
+// Class API defines the swift-polaris API.
+class API {
+    var URI: String // API lookup path
+    
+    var Accounts: AccountsAPI // Accounts API instance
+    
+    // init initializes a new API instance with the given api URI.
+    init(apiURI: String) {
+        URI = apiURI // Set URI
+        
+        Accounts = AccountsAPI(apiURI) // Initialize accounts API
+    }
+}
---
+++ swift-polaris-playground.playground/Sources/AccountsAPI.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+// Class AccounstAPI represents the accounts API.
+class AccountsAPI {
+    var URI: String // API lookup path
+    
+    // init initializes a new AccountsAPI instance.
+    init(apiURI: String) {
+        URI = apiURI // Set API URI
+    }
+}
