f8e4d9d6abc8e71bb32aaec9fe998e391867a297
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 00:11:29 -0400

    sources: Finished NewAccount() functionality.

--- swift-polaris-playground.playground/Sources/AccountsAPI.swift
+++ swift-polaris-playground.playground/Sources/AccountsAPI.swift
@@ -10,11 +10,32 @@ class AccountsAPI {
     }
     
     // NewAccount initializes a new account via the provieded API lookup URI.
-    func NewAccount() -> (String, Error) {
+    func NewAccount() -> ([String: Any]?, Error?) {
         let requestContents: [String: Any] = [:] // Empty JSON request
         
         let jsonData = try? JSONSerialization.data(withJSONObject: requestContents) // Get JSON representation
         
         var request = URLRequest(url: URL(string: String(format: "%@/NewAccount", URI))!) // Craft request
+        request.httpMethod = "post" // Set HTTP method
+        
+        request.httpBody = jsonData // Set request body
+        
+        var responseJSON: [String: Any]? = nil // Init response buffer
+        
+        var err: Error? = nil // Init error buffer
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else { // Check for errors
+                err = error! // Set error
+                
+                return // Return
+            }
+
+            responseJSON = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any] // Set response JSON
+        }
+        
+        task.resume() // Start task
+        
+        return (responseJSON, err) // Return response
     }
 }
