74bab04abe8ea9e0d4eb08a2fb56ac5200c17bb6
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 01:39:33 -0400

    source: String format method just broke for no reason ¯\_(ツ)_/¯.

--- swift-polaris-playground.playground/Sources/AccountsAPI.swift
+++ swift-polaris-playground.playground/Sources/AccountsAPI.swift
@@ -6,13 +6,44 @@ public class AccountsAPI {
     
     // init initializes a new AccountsAPI instance.
     init(apiURI: String) {
-        URI = String(format: "%@/twirp/accounts.Accounts", apiURI) // Set API URI
+        URI = "\(apiURI)/twirp/accounts.Accounts" // Set API URI
     }
     
     // NewAccount initializes a new account via the provieded API lookup URI.
     public func NewAccount() -> ([String: Any]?, Error?) {
-        let requestContents: [String: Any] = [:] // Empty JSON request
+        return Requests.MakeRequest(requestURI: "\(URI)/NewAccount", requestContents: [:]) // Return response
+    }
+    
+    // GetAllAccounts attempts to fetch the list of working accounts.
+    public func GetAllAccounts() -> ([String: Any]?, Error?) {
+        return Requests.MakeRequest(requestURI: "\(URI)/NewAccount", requestContents: [:]) // Return response
+    }
+    
+    // AccountFromKey attempts to derive an account from a given private key
+    public func AccountFromKey(key: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["PrivatePublicKey": key] // Initialize request
         
-        return Requests.MakeRequest(requestURI: String(format: "%@/NewAccount", URI), requestContents: requestContents) // Return response
+        return Requests.MakeRequest(requestURI: "\(URI)/AccountFromKey", requestContents: requestContents) // Return response
+    }
+    
+    // PublicKey attempts to derive an ECDSA public key from a given account.
+    public func PublicKey(address: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["PrivatePublicKey": address] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/PublicKey", requestContents: requestContents) // Return response
+    }
+    
+    // PublicKey attempts to derive an ECDSA private key from a given account.
+    public func PrivateKey(address: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["PrivatePublicKey": address] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/PrivateKey", requestContents: requestContents) // Return response
+    }
+    
+    // String marshals an account into a JSON-encoded string.
+    public func String(address: String) -> ([String: Any]?, Error?) {
+        let requestContents: [String: Any] = ["PrivatePublicKey": address] // Initialize request
+        
+        return Requests.MakeRequest(requestURI: "\(URI)/String", requestContents: requestContents) // Return response
     }
 }
