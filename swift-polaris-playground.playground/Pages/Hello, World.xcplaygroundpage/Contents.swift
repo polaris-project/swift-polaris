799c76a86a74adc9fe247acead1bdddc3d663e06
Dowland Aiello <mitsukomegumii@gmail.com>
2019-03-24 13:23:57 -0400

    resources: Added polaris binary.

---
+++ swift-polaris-playground.playground/Pages/Hello, World.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+/*:
+ # The API
+
+ Interacting with Polaris via Polaris-Swift is easy! Simply initialize a new API instance with a providing URI.
+    In this case, I'm initializing a new API instance with the URI for my Raspberry Pi's Go-Polaris RPC API.
+*/
+
+import Foundation
+import AppKit
+import PlaygroundSupport
+
+let api = API(apiURI: "https://108.41.124.60:8000") // Initialize API instance
+
+/*:
+ ## The Accounts API
+ 
+ When an API instance is created, so are instances of each package API. To access the accounts API, use api.Accounts.
+ 
+ ### Creating a New Account
+ 
+ Like a few other methods in the Accounts API, NewAccount() does not take any parameters. Simply call api.Accounts.NewAccount() to generate a new ECDSA private key and public key, as well as a Polaris address (derived from the Sha3 hash of the public key). Like every other API method, NewAccount() will return a JSON dict representing the response from the node, and an error (if applicable).
+*/
+
+let newAccountResponse = api.Accounts.NewAccount() // Initialize new account
+
+let newAccountAddress = newAccountResponse.0!["message"] as! String // Your new account address!
+
+print("Created new account successfully! Here's your address: \(newAccountAddress)") // Log new account
+
+/*:
+ * Callout(Forgot Your Address?):
+ Use api.Accounts.GetAllAccounts() to get a list of all the accounts created on the local node.
+*/
+
+let baseView = NSView(frame: CGRect(x: 0, y: 0, width: 500, height: 500)) // Placeholder to present address
+
+PlaygroundPage.current.liveView = baseView // Set live view
+PlaygroundPage.current.needsIndefiniteExecution = true // Keep open
+
+//: [Next](@next)
--- swift-polaris-playground.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
+++ swift-polaris-playground.playground/Pages/Welcome.xcplaygroundpage/Contents.swift
@@ -1,9 +1,29 @@
 /*:
+ # Swift-Polaris
  
+ ## An Introduction
+ 
+ I love software. I mean, I love computers in general, but software in particular is what really interests me. But, at the moment, I've noticed something fundamentally limiting about how computing works today: centralization.
+ This issue was first addressed by Satoshi Nakamoto through the creation of the Bitcoin network--a first-of-its-kind peer-to-peer digital currency. However, the Bitcoin network didn't really solve this issue of computing centralization. Another p2p currency known as Ethereum was created several years later that attempted to solve this issue. Unlike Bitcoin, Ethereum focused on distributed computations, rather than just monetary transfer. Ever since, Ethereum has reigned as the uncontested champion of distributed computing. However, in this peer-to-peer architecture that requires strict all-inclusive consensus, the network has become limited in throughput to some of its weakest nodes.
+ 
+ ## A Proposition
+ 
+ Again, as time went by and these networks grew, both of them were eventually plauged with scalability, privacy, and throughput woes. In response, I propose a solution: Polaris.
+ Polaris is a sharded, tangle-based digital currency and computations protocol. Unlike Ethereum and Bitcoin, Polaris can scale infinitely, as its consensus mechanism does not rely on the publishing of a transaction in a block, but rather the random sampling of network peers to publish the transaction. Additionally, as Polaris does not use proof-of-work as its consensus mechanism, transactions on the network do not require fees. As such, the network can be used as both a decentralized applications platform, and a data aggregation platform (e.g. IoT, data storage and hosting).
+ 
+ ### Consensus
+ 
+ Although Polaris doesn't necessarily rely on proof-of-work per say, it still needs a way to reach consensus. How is this accomplished, one might ask? Simply put, the network is randomly sampled for a volunteering peer wishing to "vouch" for a transaction. After "vouching" for a transaction, a peer is then permitted to attach one of their own transactions to the parent transaction. As such, only transactions that have been "witnessed" by another peer can be considered valid.
+ 
+ ## Implementation
+ 
+ So far, I've only implemented Polaris in Go. However, [Go-Polaris](https://github.com/polaris-project/go-polaris) has been written in such a way that different languages like swift can communicate with a node without having to run a real implementation in the language.
+ 
+ ## A Quick Note
+ 
+ This Playground will need an internet connection, as it needs to connect to the Go-Polaris RPC JSON API. If one wishes to operate without an internet connection, replace each API(URI) call with API("https://127.0.0.1:8000"). Although, in order to connect to a locally running node API, one should also run an instance of Go-Polaris locally (i.e. `go install go-polaris` or `curl https://github.com/polaris-project/go-polaris/releases/download/v0.1.0/polaris-darwin
+ 
+ ## Getting Started
+ 
+ To create your first account on the Polaris network, see [Hello, World](Hello,%20World).
 */
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
